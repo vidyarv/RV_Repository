f3c6a70b5e6f2e9cb5ec327023f617d0f2e572da
Vidya R V <vidyarv.aneesh@gmail.com>
2016-06-26 03:39:57 +0400

    Fibonacci Project

---
+++ FibonacciRecursive.java
@@ -0,0 +1,21 @@
+public class FibonacciRecursive {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+    }
+
+    public static int fibonacci(int n) {
+        // this is the method that student codes up
+        if (n < 0) {
+            return 0;
+        }
+
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
